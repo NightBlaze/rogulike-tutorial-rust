29e19dba5662d120094daaa919439a28a86f245e
Alexander Timonenkov <nightblaze1@gmail.com>
2024-02-20 22:40:46 +0300

    Add rltk package

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, RltkBuilder};
+
+struct State { }
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut rltk::prelude::Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Roguelike!");
+    }
+}
+
+fn main() -> rltk::BError {
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike game")
+        .build()?;
+    let game_state = State { };
+    rltk::main_loop(context, game_state)
 }
