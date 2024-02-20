67a2e473f359bdfdf4a21f6545b5f30552b9b648
Alexander Timonenkov <nightblaze1@gmail.com>
2024-02-20 23:10:37 +0300

    [WIP] ECS

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-use rltk::{GameState, RltkBuilder};
+use rltk::{GameState, Rltk, RltkBuilder, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State { }
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    foreground: RGB,
+    background: RGB,
+}
+
+struct State {
+    ecs: World,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut rltk::prelude::Rltk) {
@@ -13,6 +31,10 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike game")
         .build()?;
-    let game_state = State { };
+    let mut game_state = State {
+        ecs: World::new(),
+    };
+    game_state.ecs.register::<Position>();
+    game_state.ecs.register::<Renderable>();
     rltk::main_loop(context, game_state)
 }
